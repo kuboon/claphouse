cae527e7f6fd4f52c5a00011fda5cc020bc6a99a
Ohkubo KOHEI <kuboon@trick-with.net>
2021-11-08 12:17:38 +0000

    update routes

--- fresh/routes.gen.ts
+++ fresh/routes.gen.ts
@@ -2,15 +2,17 @@
 // This file SHOULD be checked into source version control.
 // To update this file, run `fresh routes`.
 
-import * as $0 from "./pages/index.tsx";
-import * as $1 from "./pages/api/joke.ts";
-import * as $2 from "./pages/[name].tsx";
+import * as $0 from "./pages/play.tsx";
+import * as $1 from "./pages/index.tsx";
+import * as $2 from "./pages/api/play.ts";
+import * as $3 from "./pages/[name].tsx";
 
 const routes = {
   pages: {
-    "./pages/index.tsx": $0,
-    "./pages/api/joke.ts": $1,
-    "./pages/[name].tsx": $2,
+    "./pages/play.tsx": $0,
+    "./pages/index.tsx": $1,
+    "./pages/api/play.ts": $2,
+    "./pages/[name].tsx": $3,
   },
   baseUrl: import.meta.url,
 };
