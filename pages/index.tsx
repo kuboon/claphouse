f5122f455e09b628b54bc7c38da2fe584616ca0d
Ohkubo KOHEI <kuboon@trick-with.net>
2021-12-02 09:37:52 +0000

    add instruction

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,22 @@ export default function Home() {
         <meta property="og:image" content={ogImage} />
       </Head>
       <CreateRoom />
+      <h2>Usage</h2>
+      <ol>
+        <li>Create a room for your online talk event</li>
+        <li>Share the URL with your attendees</li>
+        <li>Clap! Clap!</li>
+        <li>📶 Connect to the remote room. You need on after you suspend your browser.</li>
+        <li>🔊 Play sound on your local device. It also receives others at the room.</li>
+      </ol>
+      <h2>使い方</h2>
+      <ol>
+        <li>オンライントークイベント用のツールです。まずは Create Room します。</li>
+        <li>専用のURLが生成されますので、聴衆へシェア</li>
+        <li>聴衆の拍手や笑い声が全員に伝わります。</li>
+        <li>📶 ルームへ接続します。スマホをロックして復帰した後など、接続が切れることがありますのでオンにしてください。</li>
+        <li>🔊 手元の端末から音を鳴らします。ルームへ接続中は、全員の音がなります。マナーモードだと音が鳴りませんのでご確認ください。</li>
+      </ol>
     </Template>
   );
 }
